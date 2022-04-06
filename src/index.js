bd0378934f17c6db2e5066bf9ebc3a7675bcf0ad
Lekan <jimoridwanlekan@gmail.com>
2022-04-06 17:49:30 +0100

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import './style.css';
-import leaderboard from './leaderboardApi'
+import leaderboard from './leaderboardApi.js';
 
 const newGame = async () => {
-  await leaderboard.createGame()
-}
+  await leaderboard.createGame();
+};
 
 newGame();
 
-const refreshBtn = document.querySelector("#get-score");
-const submitScore = document.querySelector("#add-score");
-const nameScoreContainer = document.querySelector('ul')
-const userName = document.querySelector('#name')
-const userScore = document.querySelector('#score')
-const message = document.querySelector('#status')
+const refreshBtn = document.querySelector('#get-score');
+const submitScore = document.querySelector('#add-score');
+const nameScoreContainer = document.querySelector('ul');
+const userName = document.querySelector('#name');
+const userScore = document.querySelector('#score');
+const message = document.querySelector('#status');
 
-refreshBtn.addEventListener("click", async () => {
+refreshBtn.addEventListener('click', async () => {
   const scores = await leaderboard.getScores();
   const res = scores.result;
   nameScoreContainer.innerHTML = '';
@@ -23,26 +23,23 @@ refreshBtn.addEventListener("click", async () => {
   }
 });
 
-
 submitScore.addEventListener('click', async (e) => {
-  e.preventDefault()
+  e.preventDefault();
   const scoreType = parseInt(userScore.value, 10);
   if (userName.value !== '' && userScore.value !== '' && scoreType) {
     await leaderboard.createScore(userName.value, userScore.value);
     message.innerHTML = 'Score Added Successfully';
-    userName.value = '' 
-    userScore.value = ''
+    userName.value = '';
+    userScore.value = '';
     setTimeout(() => {
-      message.innerHTML = ''
-    }, 2000)
+      message.innerHTML = '';
+    }, 2000);
   } else {
     message.innerHTML = 'Enter score as an integer and a valid username, Please Try again';
-    userName.value = "";
-    userScore.value = "";
+    userName.value = '';
+    userScore.value = '';
     setTimeout(() => {
-      message.innerHTML = "";
+      message.innerHTML = '';
     }, 2000);
   }
-})
-
-
+});
--- src/leaderboardApi.js
+++ src/leaderboardApi.js
@@ -1,39 +1,36 @@
 let gameID = null;
-const createGame = () =>
-  fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/",
-    {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-      body: 
+const createGame = () => fetch(
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/',
+  {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body:
           JSON.stringify({
-            name: "LONEWOLF"
-      }),
-    }
-).then(res => res.json()).then(res => {
-  [... gameID] = res.result.split(' ');
+            name: 'LONEWOLF',
+          }),
+  },
+).then((res) => res.json()).then((res) => {
+  [...gameID] = res.result.split(' ');
 });
 
-const createScore = (userName, userScore) =>
-  fetch(
-    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`,
-    {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-      body: JSON.stringify({
-        user: userName,
-        score: userScore,
-      }),
-    }
-  ).then(res => res.json())
+const createScore = (userName, userScore) => fetch(
+  `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`,
+  {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify({
+      user: userName,
+      score: userScore,
+    }),
+  },
+).then((res) => res.json());
 
-const getScores = () =>
-  fetch(
-    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`
-  ).then((res) => res.json());
+const getScores = () => fetch(
+  `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`,
+).then((res) => res.json());
 
-export default {createGame, createScore, getScores}
+export default { createGame, createScore, getScores };
