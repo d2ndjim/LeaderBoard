e048b2b8072dcde2e41727d0c16a8ee90b942f67
Lekan <jimoridwanlekan@gmail.com>
2022-04-06 16:38:30 +0100

    create API file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+import leaderboard from './leaderboardApi.js'
+
+const newGame = async () => {
+  await leaderboard.createGame()
+}
+
+newGame();
+
+const refreshBtn = document.querySelector("#get-score");
+const submitScore = document.querySelector()
+
---
+++ src/leaderboardApi.js
@@ -0,0 +1,40 @@
+let gameID = ''
+
+const createGame = () =>
+  fetch(
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/",
+    {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: 
+          JSON.stringify({
+            name: "LONEWOLF"
+      })
+    }
+).then(res => res.json()).then(res => {
+  [... gameID] = res.result.split(' ');
+});
+
+const createScore = (userName, userScore) =>
+  fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`,
+    {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({
+        user: userName,
+        score: userScore,
+      }),
+    }
+  ).then(res => res.json())
+
+const getScores = () =>
+  fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`
+  ).then(res => res.json())
+
+export {createGame, createScore, getScores}
